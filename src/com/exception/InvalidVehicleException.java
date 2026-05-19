173f5d8efbc3e8d32080de2f0d398c1fc7e4439e
aakash001s <aadhidon321@gmail.com>
2026-05-19 14:18:50 +0530

    no change

--- src/com/exception/InvalidVehicleException.java
+++ src/com/exception/InvalidVehicleException.java
@@ -1,9 +1,5 @@
 package com.exception;
-import java.util.*;
 public class InvalidVehicleException {
-	public static void main(String args[]) {
-	System.out.println("hi");
-          
-          }
+	
 	
 }
