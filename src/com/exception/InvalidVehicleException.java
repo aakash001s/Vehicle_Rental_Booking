0d855e852203892cd8bc2ff9079e2feafbb496da
aakash001s <aadhidon321@gmail.com>
2026-05-19 14:01:11 +0530

    configured connector

--- src/com/exception/InvalidVehicleException.java
+++ src/com/exception/InvalidVehicleException.java
@@ -1,5 +1,9 @@
 package com.exception;
-
+import java.util.*;
 public class InvalidVehicleException {
-
+	public static void main(String args[]) {
+	System.out.println("hi");
+          
+          }
+	
 }
